4c3d61f73b53fd2fd0a995b538fa948f871e3275
James <james.hurley@newcastle.edu.au>
2026-02-19 22:03:15 +1100

    Fix Users tab: inline white text, remove debug logs

--- packages/sveltekit/src/routes/admin/Users.svelte
+++ packages/sveltekit/src/routes/admin/Users.svelte
@@ -1,25 +1,15 @@
 <script>
 	import { onMount } from 'svelte';
 
-	console.log('USERS COMPONENT LOADED');
-
 	let users = $state([]);
 
 	onMount(async () => {
-		console.log('onMount firing');
 		const res = await fetch('http://127.0.0.1:3001/user/list');
 		users = await res.json();
-		console.log('users loaded:', users.length);
 	});
 </script>
 
-<p>Users: {users.length}</p>
+<p style="color: white">Users: {users.length}</p>
 {#each users as u}
-	<p>{u.email}</p>
+	<p style="color: white">{u.email}</p>
 {/each}
-
-<style>
-	p {
-		color: white;
-	}
-</style>
